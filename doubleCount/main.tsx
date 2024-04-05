95cda183528d227372c858a58c3af9133a5596d2
Lo Min Ming <m@lominming.com>
2024-04-04 23:35:48 -0700

    add ui

--- doubleCount/main.tsx
+++ doubleCount/main.tsx
@@ -1,5 +1,10 @@
-
 function doubleCount() {
   $.set('double', $.get('count') * 2);
 }
 $.subscribe('count', doubleCount);
+
+function render() {
+  return (<div>{$.get('count')} * 2 = {$.get('double')}</div>)
+}
+
+$.set('ui', render);
